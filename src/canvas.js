b467cf2b2c833c2772fb1dbdb9fbd9b26d06f61d
KefirrOS <41384062+KefirrOS@users.noreply.github.com>
2023-01-12 15:31:58 +0100

    [main] script optimization

--- src/canvas.js
+++ src/canvas.js
@@ -1,27 +1,15 @@
-let timerID;
-
 window.onload = function() {
     canvas = document.getElementById('myCanvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
     ctx = canvas.getContext('2d');
-
     const video = document.getElementById('video');
 
-    timerID = window.setInterval( function() {
+    requestAnimationFrame(animate);
+
+    function animate(currentTime){
         ctx.drawImage(video, 5, 5, window.innerWidth, 270);
-    }, 30);
 
-    video.addEventListener('pause', function() {
-        stopTimer();
-    });
-
-    video.addEventListener('ended', function() {
-        stopTimer();
-    });
-};
-
-function stopTimer() {
-    window.clearInterval(timerID);
+        requestAnimationFrame(animate);
+    }
 };
